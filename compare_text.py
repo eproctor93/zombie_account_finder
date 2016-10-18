e8ce10ec9aab706585faaed0efc3be07884f8d00
Edward Proctor <eproctor93@users.noreply.github.com>
2016-10-18 09:53:12 +0100

    Create compare_text.py

---
+++ compare_text.py
@@ -0,0 +1,27 @@
+
+first_file_name = input("Please enter the first file name including .txt (must be in the directory)")
+second_file_name = input("Please enter the second file name including .txt (must be in the directory")
+
+print("Are these correct? Y/N", first_file_name, second_file_name)
+
+response = input()
+
+if response == 'y':
+
+    with open(first_file_name, 'r') as file1:
+
+        with open(second_file_name, 'r') as file2:
+            same = set(file1).intersection(file2)
+
+    same.discard('\n')
+
+    with open('results.txt', 'w') as file_out:
+        for line in same:
+            file_out.write(line)
+
+    num_lines = sum(1 for line in open('results.txt'))
+    print("Number of zombie accounts", num_lines)
+
+elif response == 'n':
+    print ("u ok hun")
+    exit()
